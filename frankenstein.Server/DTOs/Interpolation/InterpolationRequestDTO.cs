de99cc84b53ac678476c7911386476a37dea1fb1
Мартышов Данила Викторович <409091@niuitmo.ru>
2025-05-20 00:31:07 +0300

    Continued on l5

---
+++ frankenstein.Server/DTOs/Interpolation/InterpolationRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace frankenstein.Server.DTOs.Interpolation
+{
+    public class InterpolationRequestDTO
+    {
+        public double[] YValues { get; set; }
+        public double[] XValues { get; set; }
+        //public string Function { get; set; }
+        //public double X0 { get; set; }
+        //public double Xn { get; set; }
+        //public double N { get; set; }
+        public double TargetX { get; set; }
+
+    }
+}
